9aa4af0433d69221c101c691abb7d083235eb6e0
li <yogurt1270@163.com>
2017-07-14 00:32:06 +0800

    add

---
+++ function1.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python
+# coding:utf-8
+import pytest
+
+
+def test_case001():
+    print("case001")
+    assert 100
+
+
+def test_case002():
+    print("case002")
+    assert "case002"
